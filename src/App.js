81e70579f624b841627ecb2befb7601a018751ed
Yu Zheng <yu.zheng@inboc.net>
2021-11-17 20:13:50 +0800

    logo: change text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          This is awesome! Fantastic And Interesting
+          This is awesome! Fantastic Logo
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
